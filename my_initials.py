73538fac5a2a273e6925f083a440e365c5975448
Anmol Atmaram Desai <71127372+iamanmoldesai@users.noreply.github.com>
2022-01-17 20:04:06 -0500

    Add files via upload

---
+++ my_initials.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Anmol Desai
+#I am a coder
+
+print("  A    DDDD ")
+print(" A A   D   D")
+print("A   A  D   D")
+print("AAAAA  D   D")
+print("A   A  D   D")
+print("A   A  D   D")
+print("A   A  DDDD ")
+
+
+# In[ ]:
+
+
+
+
